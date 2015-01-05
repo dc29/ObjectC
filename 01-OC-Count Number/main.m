4130ec846f1a57f50a5a2538df0c1ec29026f013
dc29 <dc29@163.com>
2015-01-05 17:32:57 +0800

    Revert "change 2"
    This reverts commit 19a7a9c51fa503f64cf64f129ceebb1053e60351.

---
+++ 01-OC-Count Number/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  01
+//
+//  Created by admin on 15-1-5.
+//  Copyright (c) 2015年 ua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSString+DC.h"
+
+
+int main ()
+{
+
+    int len = [NSString numCountOfString:@"a1b2c3"];
+    NSLog(@"len is %d", len);
+    
+    return 0;
+}
+
+
